e709ce1fd71ceeea1b774e76ffbf0d5677ea328d
Natalia <y_natalya_v@mail.ru>
2022-02-07 23:06:32 +0300

    правки

--- source/js/script.js
+++ source/js/script.js
@@ -1,11 +1,24 @@
-const toggleBtn = document.querySelector('.page-header__toggle');
-const toggleNav = document.querySelector('.page-header--closed');
+const toggleBtn = document.querySelector('.page-header__btn');
+const toggleNav = document.querySelector('.header-nav');
+const noJsElement = document.querySelector('.page-header--nojs');
 
-const showMenu = () => {
-  toggleBtn.addEventListener('click', () => {
-    toggleNav.classList.toggle('page-header--closed');
-    toggleNav.classList.toggle('page-header--opened');
-  })
+const checkJs = () => {
+  if (noJsElement) {
+    noJsElement.classList.remove('page-header--nojs');
+  }
 }
 
-showMenu();
+const toggleMenu = () => {
+  toggleNav.classList.toggle('page-header--closed');
+  toggleBtn.classList.toggle('header-nav__toggle--closed');
+}
+
+const initMenu = () => {
+  toggleNav.classList.add('page-header--closed');
+  toggleBtn.classList.add('header-nav__toggle--closed');
+}
+
+checkJs();
+initMenu();
+
+toggleBtn.addEventListener('click', toggleMenu);
